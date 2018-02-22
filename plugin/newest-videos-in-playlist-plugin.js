5093a7220d036b5543e79f35363794489a941ae5
Jeff Doktor <jdoktor@brightcove.com>
2018-02-22 11:42:51 -0700

    update CMS API proxy call

--- plugin/newest-videos-in-playlist-plugin.js
+++ plugin/newest-videos-in-playlist-plugin.js
@@ -1,80 +1,87 @@
 videojs.registerPlugin('newestVideosInPlaylist', function() {
 
   var myPlayer= this,
-    proxyURL = 'https://solutions.brightcove.com/bcls/bcls-proxy/doc-samples-proxy.php',
-    cmsURL = 'https://cms.api.brightcove.com/v1/accounts/',
     requestData = {},
       newestVideos = [],
       videoData = [],
-      videoObjects = [];
+      videoObjects = [],
+      // Build options needed for CMS API request
+      options = {},
+      baseURL = "https://cms.api.brightcove.com/v1/accounts/",
+      accountId = "1752604059001";
 
-    // +++ set up data for CMS API request +++
-    requestData = setRequestData();
+  options.proxyURL = "https://solutions.brightcove.com/bcls/bcls-proxy/brightcove-learning-proxy-v2.php";
+  options.requestType = "GET";
+  // return the 10 newest videos created
+  endPoint = "?sort=-created_at&limit=10";
+  options.url = baseURL + accountId + "/videos" + endPoint;;
 
     // +++ make the CMS API request to get matching video IDs +++
-    getNewestVideos(requestData, function(newestVideos) {
+    makeRequest(options, function (newestVideos) {
+      // Convert response string into JSON
+      JSONnewestVideos = JSON.parse(newestVideos);
       // extract the needed video data into an array
-      videoData = extractVideoData(newestVideos);
+      videoData = extractVideoData(JSONnewestVideos);
       getVideoData(videoData, function(videoObjects) {
         // add the newest videos list to the player as a playlist
         myPlayer.playlist(videoObjects);
       });
     });
 
-  // +++ Sets up the data for the API request +++
-  function setRequestData() {
-    var endPoint = '',
-      accountId = '1752604059001',
-      videoName,
-      requestURL,
-      endPoint,
-      requestData = {},
-      dataReturned = false;
-    requestURL = cmsURL + accountId + '/videos';
-    // return the 10 newest videos created
-    endPoint = '?sort=-created_at&limit=10';
-    requestData.url = requestURL + endPoint;
-    requestData.requestType = 'GET';
-    return requestData;
-  }
-
-  // +++ Makes actual call for data +++
-  function getNewestVideos(options, callback) {
-    var httpRequest = new XMLHttpRequest(),
-      responseRaw,
-      parsedData,
-      requestParams;
-    // set up request data
-    requestParams = 'url=' + encodeURIComponent(options.url) + '&requestType=' + options.requestType;
-    // set response handler
-    httpRequest.onreadystatechange = getResponse;
-    // open the request
-    httpRequest.open('POST', proxyURL);
-    // set headers
-    httpRequest.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
-    // open and send request
-    httpRequest.send(requestParams);
-    // response handler
-    function getResponse() {
-      dataReturned = false;
-      try {
-        if (httpRequest.readyState === 4) {
-          if (httpRequest.status === 200) {
-            responseRaw = httpRequest.responseText;
-            parsedData = JSON.parse(responseRaw);
-            dataReturned = true;
-          } else {
-            alert('There was a problem with the request. Request returned ' + httpRequest.status);
+    // +++ Make the CMS API request +++
+    /**
+     * send API request to the proxy
+     * @param  {Object} options for the request
+     * @param  {String} options.url the full API request URL
+     * @param  {String="GET","POST","PATCH","PUT","DELETE"} requestData [options.requestType="GET"] HTTP type for the request
+     * @param  {String} options.proxyURL proxyURL to send the request to
+     * @param  {String} options.client_id client id for the account (default is in the proxy)
+     * @param  {String} options.client_secret client secret for the account (default is in the proxy)
+     * @param  {JSON} [options.requestBody] Data to be sent in the request body in the form of a JSON string
+     * @param  {Function} [callback] callback function that will process the response
+     */
+    function makeRequest(options, callback) {
+      var httpRequest = new XMLHttpRequest(),
+        response,
+        requestParams,
+        dataString,
+        proxyURL = options.proxyURL,
+        // response handler
+        getResponse = function() {
+          try {
+            if (httpRequest.readyState === 4) {
+              if (httpRequest.status >= 200 && httpRequest.status < 300) {
+                response = httpRequest.responseText;
+                // some API requests return '{null}' for empty responses - breaks JSON.parse
+                if (response === "{null}") {
+                  response = null;
+                }
+                // return the response
+                callback(response);
+              } else {
+                alert(
+                  "There was a problem with the request. Request returned " +
+                    httpRequest.status
+                );
+              }
+            }
+          } catch (e) {
+            alert("Caught Exception: " + e);
           }
-        }
-      } catch (e) {
-        alert('Caught Exception: ' + e);
-      }
-      if (dataReturned) {
-        callback(parsedData);
-      }
+        };
+      /**
+       * set up request data
+       * the proxy used here takes the following request body:
+       * JSON.strinify(options)
+       */
+      // set response handler
+      httpRequest.onreadystatechange = getResponse;
+      // open the request
+      httpRequest.open("POST", proxyURL);
+      // set headers if there is a set header line, remove it
+      // open and send request
+      httpRequest.send(JSON.stringify(options));
     }
-  }
 
   // +++ Standard code to extract videos from returned data +++
   /**
